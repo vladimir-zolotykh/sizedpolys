07b71783c146bca7593863e0d53fbc4f4c111bb1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-04-04 20:42:19 +0300

    Add read_records, test_write_records

--- write_records.py
+++ write_records.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 from typing import BinaryIO
-from struct import Struct
+from struct import Struct, calcsize
+from functools import partial
 
 RecordType = tuple[int, float, float]
 
@@ -13,11 +14,22 @@ def write_records(records: list[RecordType], format: str, f: BinaryIO):
         f.write(s.pack(*r))
 
 
-if __name__ == "__main__":
-    records = [
+def read_records(format: str, f: BinaryIO) -> list[RecordType]:
+    s = Struct(format)
+    size = calcsize(format)
+    return [s.unpack(r) for r in iter(partial(f.read, size), b"")]
+
+
+def test_write_records(tmp_path) -> None:
+    records: list[RecordType] = [
         (1, 2.3, 4.5),
         (6, 7.8, 9.0),
         (12, 13.4, 56.7),
     ]
-    with open("data.bin", "wb") as f:
-        write_records(records, "<idd", f)
+    fmt = "<idd"
+    file_path = tmp_path / "data.bin"
+    with open(file_path, "wb") as f:
+        write_records(records, fmt, f)
+    with open(file_path, "rb") as f:
+        got_records = read_records(fmt, f)
+    assert records == got_records

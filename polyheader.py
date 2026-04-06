9120061776e10a870aba527cb03c9f7fd56adf1a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-04-06 10:56:23 +0300

    Initial import
    classes Descriptor, Structure, PolyHeader and main entry to test.

---
+++ polyheader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from struct import Struct
+
+
+class Descriptor:
+    def __init__(self, format: str, offset: int):
+        self._name = None
+        self._format = format
+        self._struct = Struct(format)
+        self._offset = offset
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner=None):
+        if self is None:
+            return self
+        return self._struct.unpack_from(instance._buffer, self._offset)
+
+
+class Structure:
+    def __init__(self, bytedata):
+        self._buffer = memoryview(bytedata)
+
+
+class PolyHeader(Structure):
+    code = Descriptor("<i", 0)
+    minx = Descriptor("<d", 4)
+    miny = Descriptor("<d", 12)
+    maxx = Descriptor("<d", 20)
+    maxy = Descriptor("<d", 28)
+    npolys = Descriptor("<i", 36)
+
+
+if __name__ == "__main__":
+    from read_polys import polys, make_polysdata, write_polys
+
+    polysdata = make_polysdata(polys)
+    write_polys("polys.bin", polysdata)
+    with open("polys.bin", "rb") as f:
+        buffer = f.read()
+        polyheader = PolyHeader(buffer)
+    print(polyheader.code)
+    print(polyheader.minx)
+    print(polyheader.miny)
+    print(polyheader.maxx)
+    print(polyheader.maxy)
+    print(polyheader.npolys)
